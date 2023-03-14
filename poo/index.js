01b793ef9ba5fc2680597d6f3eb9ec18d2352b1e
thomillaz <kamomillathomaz@gmail.com>
2023-03-13 23:06:30 -0300

    estudo de módulos js e package.json

--- poo/index.js
+++ poo/index.js
@@ -1,21 +1,23 @@
-class Cliente{
-    nome;
-    cpf;
-    agencia;
-    saldo;
-}
+import { Cliente } from './Cliente.js';
+import { ContaCorrente } from './ContaCorrente.js';
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
-cliente1.nome = 'Ricardo';
+cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
-cliente2.nome = 'Alice';
-cliente2.cpf = '88822233309';
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
 
-console.log(cliente1, cliente2);
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(-100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
