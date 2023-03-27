3c6cf0a12c2825bb1420c4fa83dcd276d6717d98
thomillaz <kamomillathomaz@gmail.com>
2023-03-26 23:18:06 -0300

    Conclusão curso JS - POO do Programa ONE

--- poo/index.js
+++ poo/index.js
@@ -1,17 +1,10 @@
 import { Cliente } from './Cliente.js';
 import { ContaCorrente } from './ContaCorrente.js';
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 
 contaCorrenteRicardo.depositar(-100);
 contaCorrenteRicardo.depositar(100);
@@ -21,3 +14,18 @@ const valorSacado = contaCorrenteRicardo.sacar(50);
 console.log(valorSacado);
 
 console.log(contaCorrenteRicardo);
+
+const conta2 = new ContaCorrente(cliente2, 102);
+console.log(conta2.cliente);
+
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.transferir(200, conta2);
+
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
+
+console.log("valor: ", valor);
+console.log(conta2.saldo);
+console.log(contaCorrenteRicardo);
+
+console.log(`Número de contas: ${ContaCorrente.numeroDeContas}`);
